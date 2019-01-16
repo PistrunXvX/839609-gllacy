9ef7e6ba8ad48f07a69d9d859dedf805b6e9d313
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2019-01-16 15:53:00 +0300

    final

--- js/script.js
+++ js/script.js
@@ -20,13 +20,13 @@ linkClose.addEventListener("click", function (evt)  {
 const linkOpenUnder = document.querySelector(".under-item-btn");
 const popUpUnder = document.querySelector(".under-item-wrapper");
 
-linkOpenUnder.addEventListener("click", function (evt) {
+linkOpenUnder.addEventListener("mouseover", function (evt) {
   evt.preventDefault();
   console.log("Get");
   popUpUnder.classList.add("pop-up-show");
 });
 
-linkOpenUnder.addEventListener("mouseover", function (evt) {
+linkOpenUnder.addEventListener("click", function (evt) {
   evt.preventDefault();
   console.log("Close");
   popUpUnder.classList.remove("pop-up-show");
@@ -35,13 +35,13 @@ linkOpenUnder.addEventListener("mouseover", function (evt) {
 const linkOpenFind = document.querySelector(".btn-search-js");
 const popUpFind = document.querySelector(".pop-up-find-js");
 
-linkOpenFind.addEventListener("click", function (evt) {
+linkOpenFind.addEventListener("mouseover", function (evt) {
   evt.preventDefault();
   console.log("Get");
   popUpFind.classList.add("pop-up-show");
 });
 
-linkOpenFind.addEventListener("mouseover", function (evt) {
+linkOpenFind.addEventListener("click", function (evt) {
   evt.preventDefault();
   console.log("Close");
   popUpFind.classList.remove("pop-up-show");
@@ -50,13 +50,13 @@ linkOpenFind.addEventListener("mouseover", function (evt) {
 const linkOpenEnter = document.querySelector(".btn-enter-js");
 const popUpEnter = document.querySelector(".pop-up-enter-js");
 
-linkOpenEnter.addEventListener("click", function (evt) {
+linkOpenEnter.addEventListener("mouseover", function (evt) {
   evt.preventDefault();
   console.log("Get");
   popUpEnter.classList.add("pop-up-show");
 });
 
-linkOpenEnter.addEventListener("mouseover", function (evt) {
+linkOpenEnter.addEventListener("click ", function (evt) {
   evt.preventDefault();
   console.log("Close");
   popUpEnter.classList.remove("pop-up-show");
